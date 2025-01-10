e3c4d68ec5476846bffef70b590f634f0106de80
Shreyash Gajanan Jadhav <sj2454925@gmail.com>
2025-01-10 08:48:53 +0530

    on submitting the login and register

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -12,6 +12,7 @@ export default function LoginPage({ setShowSearchAndLogout }) {
   const dispatcher = useDispatch();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const [showPassword, setShowPassword] = useState(false);
 
@@ -31,6 +32,7 @@ export default function LoginPage({ setShowSearchAndLogout }) {
 
     let jwttoken = null;
     const backendUrl = process.env.REACT_APP_BACKEND_URL;
+    setLoading(true);
     await axios({
       url: `${backendUrl}/generate/authenticate`,
       method: "POST",
@@ -48,6 +50,7 @@ export default function LoginPage({ setShowSearchAndLogout }) {
         console.log("ERROR occured" + err);
         setError(true);
       });
+    setLoading(false);
     console.dir(jwttoken);
     if (jwttoken != null) {
       navigate(`/main`);
@@ -56,83 +59,86 @@ export default function LoginPage({ setShowSearchAndLogout }) {
     }
   };
   return (
-    <div className="h-screen flex justify-evenly fixed top-0 left-0 w-full">
-      <form
-        onSubmit={onSubmit}
-        className="md:flex m-auto border-2 rounded-md shadow-2xl overflow-hidden"
-      >
-        <div className="flex flex-col px-8 py-12 gap-6">
-          <label className="input input-bordered flex items-center gap-2 h-12 w-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-6 w-6 opacity-70"
-            >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-            </svg>
-            <input
-              onChange={(event) => setUsername(event.target.value)}
-              placeholder="username"
-              className="flex-1 h-full px-4 input-lg border-0 outline-none"
-            ></input>
-          </label>
-          <label className="relative input input-bordered flex items-center gap-2 h-12 w-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-6 w-6 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <input
-              onChange={(event) => setPassword(event.target.value)}
-              placeholder="password"
-              className="flex-1 h-full px-4 input-lg border-0 outline-none "
-              type={`${showPassword ? "text" : "password"}`}
-            ></input>
-            {!showPassword ? (
-              <FaEye
-                className="absolute top-4 right-4 text-md"
-                onClick={() => setShowPassword(true)}
-              ></FaEye>
-            ) : (
-              <FaRegEyeSlash
-                className="absolute top-4 right-4 text-md"
-                onClick={() => setShowPassword(false)}
-              ></FaRegEyeSlash>
-            )}
-          </label>
-          <div className="flex">
-            <p className="text-sm text-slate-600">haven't registered please </p>
-            <a
-              className="text-sm text-blue-600 ml-3 hover:underline cursor-pointer"
-              onClick={() => navigate("/register")}
-            >
-              register
-            </a>
-          </div>
-          {/*below error tag */}
-          {error ? (
-            <p className="text-red-600 text-sm">
-              Invalid credentials or user doesn't exist !{" "}
-            </p>
-          ) : (
-            ""
-          )}
-        </div>
-        <button
-          className=" w-full bg-gray-800 py-5 hover:underline text-white md:w-[100px] md:rounded-tr-md md:rounded-br-md"
-          onClick={onSubmit}
+      <div className="h-screen flex justify-evenly fixed top-0 left-0 w-full">
+        {loading?
+        <span className="fixed top-[50%] left-[50%] loading loading-spinner loading-lg mx-auto"></span>:
+        <form
+            onSubmit={onSubmit}
+            className="md:flex m-auto border-2 rounded-md shadow-2xl overflow-hidden"
         >
-          LOGIN
-        </button>
-      </form>
-    </div>
+          <div className="flex flex-col px-8 py-12 gap-6">
+            <label className="input input-bordered flex items-center gap-2 h-12 w-full">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-6 w-6 opacity-70"
+              >
+                <path
+                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"/>
+              </svg>
+              <input
+                  onChange={(event) => setUsername(event.target.value)}
+                  placeholder="username"
+                  className="flex-1 h-full px-4 input-lg border-0 outline-none"
+              ></input>
+            </label>
+            <label className="relative input input-bordered flex items-center gap-2 h-12 w-full">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-6 w-6 opacity-70"
+              >
+                <path
+                    fillRule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clipRule="evenodd"
+                />
+              </svg>
+              <input
+                  onChange={(event) => setPassword(event.target.value)}
+                  placeholder="password"
+                  className="flex-1 h-full px-4 input-lg border-0 outline-none "
+                  type={`${showPassword ? "text" : "password"}`}
+              ></input>
+              {!showPassword ? (
+                  <FaEye
+                      className="absolute top-4 right-4 text-md"
+                      onClick={() => setShowPassword(true)}
+                  ></FaEye>
+              ) : (
+                  <FaRegEyeSlash
+                      className="absolute top-4 right-4 text-md"
+                      onClick={() => setShowPassword(false)}
+                  ></FaRegEyeSlash>
+              )}
+            </label>
+            <div className="flex">
+              <p className="text-sm text-slate-600">haven't registered please </p>
+              <a
+                  className="text-sm text-blue-600 ml-3 hover:underline cursor-pointer"
+                  onClick={() => navigate("/register")}
+              >
+                register
+              </a>
+            </div>
+            {/*below error tag */}
+            {error ? (
+                <p className="text-red-600 text-sm">
+                  Invalid credentials or user doesn't exist !{" "}
+                </p>
+            ) : (
+                ""
+            )}
+          </div>
+          <button
+              className=" w-full bg-gray-800 py-5 hover:underline text-white md:w-[100px] md:rounded-tr-md md:rounded-br-md"
+              onClick={onSubmit}
+          >
+            LOGIN
+          </button>
+        </form>}
+      </div>
   );
 }
