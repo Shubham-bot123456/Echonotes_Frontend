cf2a46195828949315a7b342ce36ea9d1d2eaf40
Shreyash Gajanan Jadhav <sj2454925@gmail.com>
2025-01-14 10:17:55 +0530

    animation added to usermodal component

--- src/components/ShareModal.jsx
+++ src/components/ShareModal.jsx
@@ -7,6 +7,14 @@ import { motion, useAnimation } from "framer-motion";
 export default function ShareModal({ shareBook, book, setBlurr }) {
     const animation = useAnimation();
     const [calledUser, setCalledUser] = useState("");
+    // animation.start({
+    //     scale: 1,
+    //     opacity: 1,
+    //     translateY:0,
+    //     transition: {
+    //         duration:0.4 ,
+    //     }
+    // })
 
     const openModal = () => {
         const modal = document.getElementById(`shareModal${book.id}`);
--- src/components/UsersModal.jsx
+++ src/components/UsersModal.jsx
@@ -1,83 +1,85 @@
-import React, { useRef } from "react";
-import { MdOutlinePeople } from "react-icons/md";
-import { RxCross1 } from "react-icons/rx";
+import React, {useRef} from "react";
+import {MdOutlinePeople} from "react-icons/md";
+import {RxCross1} from "react-icons/rx";
 import avatarNameToImageFunction from "./AvatarImageHelper";
-import { useSelector } from "react-redux";
-import { motion, AnimatePresence, useAnimation } from "framer-motion";
+import {useSelector} from "react-redux";
+import {motion, AnimatePresence, useAnimation} from "framer-motion";
 
-export default function UserModal({ users, bookId }) {
-  const loginUser = useSelector((state) => state.userdetails.value);
-  const animation = useAnimation();
-  return (
-    <div>
-      <MdOutlinePeople
-        className="absolute top-4 right-4 text-xl hover:scale-105"
-        onClick={(e) => {
-          console.log(
+export default function UserModal({users, bookId}) {
+    const loginUser = useSelector((state) => state.userdetails.value);
+    const animation = useAnimation();
+
+    const openUserModal = (e) => {
+        console.log(
             "inside the people button and the users to display are " + users
-          );
-          console.log("inside the people button and the book id is " + bookId);
-          e.stopPropagation();
-          document.getElementById(`user_modal${bookId}`).showModal();
-
-          animation.start({
+        );
+        console.log("inside the people button and the book id is " + bookId);
+        e.stopPropagation();
+        document.getElementById(`user_modal${bookId}`).showModal();
+        animation.start({
             opacity: 1,
             scale: 1,
             translateY: 0,
-            transition: { duration: 0.4 },
-          });
-        }}
-      />
-
-      <motion.dialog id={`user_modal${bookId}`} className="modal ">
-        <motion.div
-          animate={animation}
-          initail={{
+            transition: {duration: 0.4},
+        });
+    }
+    const closeUserModal = (e) => {
+        e.stopPropagation();
+        document.getElementById(`user_modal${bookId}`).close();
+        animation.start({
             opacity: 0,
-            translateY: 200,
             scale: 0,
-          }}
-          className="relative modal-box w-[60vw] h-[40vh] scroll-auto rounded-md "
-          onClick={(e) => e.stopPropagation()}
-        >
-          <section className="flex gap-2">
-            <MdOutlinePeople className="text-2xl my-auto"></MdOutlinePeople>
-            <h1 className="Shared with:  text-md ">Shared with : </h1>
-          </section>
-          <section>
-            <ul onClick={(e) => e.stopPropagation()} className="my-4">
-              {users.map((user) => {
-                if (user.username === loginUser) return;
-                return (
-                  <li
-                    className="w-full py-4 px-10 my-2 rounded-md shadow-2xl text-center flex gap-5 "
-                    key={user.username}
-                  >
-                    <div className="w-8 my-auto">
-                      {avatarNameToImageFunction(user.avatar)}
-                    </div>
-                    <p className="text-md">{user.username}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </section>
+            transition: {duration: 0.4},
+        });
+    }
+    return (
+        <div>
+            <MdOutlinePeople
+                className="absolute top-4 right-4 text-xl hover:scale-105"
+                onClick={openUserModal}
+            />
 
-          <RxCross1
-            className=" absolute top-6 right-5 text-2xl hover:scale-105"
-            onClick={(e) => {
-              e.stopPropagation();
-              document.getElementById(`user_modal${bookId}`).close();
-              animation.start({
-                opacity: 0,
-                scale: 0,
-                translateY: 200,
-                transition: { duration: 0.5 },
-              });
-            }}
-          ></RxCross1>
-        </motion.div>
-      </motion.dialog>
-    </div>
-  );
+            <dialog id={`user_modal${bookId}`} className="modal" >
+                    <motion.div
+                        animate={animation}
+                        initial={{
+                            opacity: 0,
+                            translateY: 200,
+                            scale: 0,
+                        }}
+                        className="relative modal-box w-[60vw] h-[40vh] scroll-auto rounded-md "
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <section className="flex gap-2">
+                            <MdOutlinePeople className="text-2xl my-auto"></MdOutlinePeople>
+                            <h1 className="Shared with:  text-md ">Shared with : </h1>
+                        </section>
+                        <section>
+                            <ul onClick={(e) => e.stopPropagation()} className="my-4">
+                                {users.map((user) => {
+                                    if (user.username === loginUser) return;
+                                    return (
+                                        <li
+                                            className="w-full py-4 px-10 my-2 rounded-md shadow-2xl text-center flex gap-5 "
+                                            key={user.username}
+                                        >
+                                            <div className="w-8 my-auto">
+                                                {avatarNameToImageFunction(user.avatar)}
+                                            </div>
+                                            <p className="text-md">{user.username}</p>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </section>
+
+                        <RxCross1
+                            className=" absolute top-6 right-5 text-2xl hover:scale-105"
+                            onClick={closeUserModal}
+                        ></RxCross1>
+                    </motion.div>
+
+            </dialog>
+        </div>
+    );
 }
